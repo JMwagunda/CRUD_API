ce09bfc1c9b0a61f55151990e8957dc0fe7b5bdb
JMwagunda <wagundamarc@gmail.com>
2024-03-15 01:54:12 +0300

    corrected errors in  product.route.js

--- routes/product.route.js
+++ routes/product.route.js
@@ -1,7 +1,6 @@
 const express = require("express");
-// const Product = require("../models/product.model.js");
 const router = express.Router();
-const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require("../controllers/product.controller.js");
+const {getProducts, getProduct, createProduct, updateProduct, deleteProduct} = require("../controllers/product.Controller.js");
 
 router.get('/', getProducts);
 router.get("/:id", getProduct);
